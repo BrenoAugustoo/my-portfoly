662e939eb46cbc9d8a3b7349f111f715e3657453
BrenoAugustoo <brenoaugusto98@gmail.com>
2023-06-12 23:08:21 -0300

    sessão conhecimentos

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -20,8 +20,8 @@ const MOCK_CONTACTS = [
 ]
 
 export const HeroSection = () => {
-  return (
-    <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px] " >
+  return (   
+    <section className="w-full lg:h-[755px] bg-hero-  image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px] " >
       <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
         <div className="w-full lg:max-w-[530px]" >
           <p className="font-mono text-emerald-400" >Olá, meu nome é</p>
@@ -31,7 +31,7 @@ export const HeroSection = () => {
           <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
             {Array.from({length: 5}).map((_, index) => ( 
               <TechBadge 
-                key={index}
+                key={index} 
                 name="Next.js"
               />
             ))}
